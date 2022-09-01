9e49c3c67e738c660b43bcbd0eb25e336409dd52
kayconfig <odoleemma@gmail.com>
2022-09-01 17:34:09 +0100

    update .env.sample, package.json

---
+++ src/common/interceptors/wrap-response.interceptor.ts
@@ -0,0 +1,15 @@
+import {
+  CallHandler,
+  ExecutionContext,
+  Injectable,
+  NestInterceptor,
+} from '@nestjs/common';
+import { map, Observable } from 'rxjs';
+
+@Injectable()
+export class WrapResponseInterceptor implements NestInterceptor {
+  intercept(context: ExecutionContext, next: CallHandler): Observable<any> {
+    // modify the response returned from handlers
+    return next.handle().pipe(map((data) => ({ data })));
+  }
+}
